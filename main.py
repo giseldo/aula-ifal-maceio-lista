2e50608c5a51ad7867624f9db32f3fd2509c5702
Giseldo <giseldo@gmail.com>
2025-11-17 20:43:01 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# Tarefa crie uma lista parcial (recurso slice) com somente 3 meses, a partir do mes de agosto, 
+# ou seja agosto, setembro e outubro
+# imprima o valor na saída principal 
+
+# meses_ano = ("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", 
+#              "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+
+# for mes in meses_ano[7:10]:
+#      print(mes)
+
+
+# Leia 4 notas e salve em uma lista (dica: list [] input)
+# Verifique se ele quer alterar (print while input)
+# Depois salve em uma tupla ( tuple () )
+# Calcule a média da tupla e imprima na saída padrão (operações básicas + / 4)
+# Tente alterar um valor da tupla e capture a exeção com uma mensagem padrão (try except ValueError)
+
+num1 = int(input("Digite a nota 1:"))
+num2 = int(input("Digite a nota 2:"))
+num3 = int(input("Digite a nota 3:"))
+num4 = int(input("Digite a nota 4:"))
+notas = [num1, num2, num3, num4]
+
+deseja_alterar = 1
+while (deseja_alterar == 1):
+    deseja_alterar = int(input("Vc desejar alterar alguma nota? (0 - Nao ; 1 -Sim)"))
+    if (deseja_alterar == 1):
+        indice_nota = input("Qual a nota? (1, 2, 3 ou 4)")
+        valor_alterado_nota = input("Informe a nova nota:")
+        notas[int(indice_nota) - 1] = valor_alterado_nota
+tupla_notas = tuple(notas) # converte lista em tuplas
+print("Média: ", (tupla_notas[0] + tupla_notas[1] + tupla_notas[2] + tupla_notas[3])/4) # imprime a média
+
+try:
+    tupla_notas[0] = 10
+except TypeError as e:
+    print("não é possivel alterar a  nota depois do fim do bimestre")
+
+    
+
+
+
+
+
+
+
+
