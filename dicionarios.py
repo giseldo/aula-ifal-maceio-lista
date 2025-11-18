39a4fbcb17b08fd8944cdd8620e24bdea0691bf9
Giseldo <giseldo@gmail.com>
2025-11-17 21:30:47 -0300

    ultim versao

---
+++ dicionarios.py
@@ -0,0 +1,51 @@
+# aluno = {
+#     "nome": "Fulano",
+#     "idade": 16,
+#     "nota": 9.5
+# }
+
+# print(aluno["nome"])
+# print(aluno["idade"])
+# print(aluno["nota"])
+
+# alunos = [
+#     {"nome": "Ana", "notas": (10, 20, 30, 40)},
+#     {"nome": "Jose", "notas": (10, 20, 30, 40)}
+# ]
+
+# for aluno in alunos:
+#     media = (sum(aluno["notas"])/ len(aluno["notas"]))
+#     print(aluno["nome"]) 
+
+''''
+Faça um dicionario que possui a seguinte estrutura:
+nome
+cpf
+endereco (rua, numero, bairro, cidade, UF)
+
+Adicione ao menos 3 linhas nesse dicionário, preechendo todos os dados.
+
+Imprima os dados de cada pessoa, como:
+
+Pessoa: {nome}
+CPF: {cpf}
+Endereço: {rua}, {numero}, {bairro}. {cidade} - {UF}.
+'''
+
+# Pessoa = {
+#     "nome": "giseldo",
+#     "cpf": "1234567",
+#     "endereco": {"rua": "Rua A", "bairro": "Universitario", "cidade": "Maceio", "UF": "AL"}
+# }
+
+
+
+lista_1 = list()
+
+meu_dict = dict()
+meu_dict["nome"] = "giseldo"
+meu_dict["cpf"] = "123"
+
+lista_1.append(meu_dict)
+lista_1.append(meu_dict)
+
--- tuplas.py
+++ tuplas.py
@@ -1,4 +1,4 @@
-# Tarefa crie uma lista parcial (recurso slice) com somente 3 meses, a partir do mes de agosto, 
+# Tarefa 1  crie uma lista parcial (recurso slice) com somente 3 meses, a partir do mes de agosto, 
 # ou seja agosto, setembro e outubro
 # imprima o valor na saída principal 
 
@@ -8,25 +8,23 @@
 # for mes in meses_ano[7:10]:
 #      print(mes)
 
-
+# Tarefa 2
 # Leia 4 notas e salve em uma lista (dica: list [] input)
 # Verifique se ele quer alterar (print while input)
 # Depois salve em uma tupla ( tuple () )
 # Calcule a média da tupla e imprima na saída padrão (operações básicas + / 4)
 # Tente alterar um valor da tupla e capture a exeção com uma mensagem padrão (try except ValueError)
 
-num1 = int(input("Digite a nota 1:"))
-num2 = int(input("Digite a nota 2:"))
-num3 = int(input("Digite a nota 3:"))
-num4 = int(input("Digite a nota 4:"))
-notas = [num1, num2, num3, num4]
-
+notas = []
+for i in range(4):
+    notas.append(int(input("Digite a nota: ")))
+    
 deseja_alterar = 1
 while (deseja_alterar == 1):
-    deseja_alterar = int(input("Vc desejar alterar alguma nota? (0 - Nao ; 1 -Sim)"))
+    deseja_alterar = int(input("Vc desejar alterar alguma nota? (0 - Nao ; 1 - Sim): "))
     if (deseja_alterar == 1):
-        indice_nota = input("Qual a nota? (1, 2, 3 ou 4)")
-        valor_alterado_nota = input("Informe a nova nota:")
+        indice_nota = input("Digite o bimestre (1, 2, 3 ou 4): ")
+        valor_alterado_nota = input("Informe a nova nota: ")
         notas[int(indice_nota) - 1] = valor_alterado_nota
 tupla_notas = tuple(notas) # converte lista em tuplas
 print("Média: ", (tupla_notas[0] + tupla_notas[1] + tupla_notas[2] + tupla_notas[3])/4) # imprime a média
