cecdddfd07cd522f52305bfb613aad4b6734f723
Giseldo <giseldo@gmail.com>
2025-11-17 21:40:10 -0300

    ultima versao

--- dicionarios.py
+++ dicionarios.py
@@ -32,10 +32,30 @@ CPF: {cpf}
 Endereço: {rua}, {numero}, {bairro}. {cidade} - {UF}.
 '''
 
-# Pessoa = {
-#     "nome": "giseldo",
-#     "cpf": "1234567",
-#     "endereco": {"rua": "Rua A", "bairro": "Universitario", "cidade": "Maceio", "UF": "AL"}
-# }
+pessoas = [
+    {
+     "nome": "giseldo",
+     "cpf": "1234567",
+     "endereco": {"rua": "Rua A", "bairro": "Universitario", "cidade": "Maceio", "UF": "AL"}
+    },
 
+    {
+     "nome": "Alex",
+     "cpf": "1234",
+     "endereco": {"rua": "Rua A", "bairro": "Universitario", "cidade": "Maceio", "UF": "AL"}
+    },
 
+    {
+     "nome": "Jose",
+     "cpf": "1234",
+     "endereco": {"rua": "Rua A", "bairro": "Universitario", "cidade": "Maceio", "UF": "AL"}
+    },
+
+]
+
+for pessoa in pessoas:
+    print(f"""
+          Nome: {pessoa["nome"]}, CPF: {pessoa["cpf"]}, 
+          Endereço:
+          Rua: {pessoa["endereco"]["rua"]}, Bairro: {pessoa["endereco"]["bairro"]}, Bairro: {pessoa["endereco"]["cidade"]}, Bairro: {pessoa["endereco"]["UF"]}
+          """)
